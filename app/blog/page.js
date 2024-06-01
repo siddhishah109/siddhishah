a5c2d5d1d76af0efeefd2870114c206022f2919e
Siddhi Shah <shahsiddhi777@gmail.com>
2024-06-01 18:06:04 +0530

    All project page added

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,21 +1,10 @@
 // @flow strict
 
-import { personalData } from "@/utils/data/personal-data";
-import BlogCard from "../components/homepage/projects/project-card";
+import { allProjectsData } from "@/utils/data/all-projects-data";
+import ProjectCard from "../components/homepage/projects/project-card";
 
-async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-  return data;
-};
-
-async function page() {
-  const blogs = await getBlogs();
+ function page() {
+ 
 
   return (
     <div className="py-8">
@@ -23,17 +12,17 @@ async function page() {
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Blog
+            All Projects
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
-        {
-          blogs.map((blog, i) => (
-            blog?.cover_image &&
-            <BlogCard blog={blog} key={i} />
+         {
+          allProjectsData.map((project, i) => (
+            project?.image &&
+            <ProjectCard project={project} key={i} />
           ))
         }
       </div>
