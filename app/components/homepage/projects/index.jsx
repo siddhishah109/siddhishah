04178a8304321570b658fcf6e8d6c9e0a2cfe456
Siddhi Shah <shahsiddhi777@gmail.com>
2024-06-01 18:24:32 +0530

    Refactor project page component and fix typo in achievements component

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -6,7 +6,7 @@ import { projectsData } from "@/utils/data/projects-data";
 function Projects() {
 
   return (
-    <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div id='projects' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
 
       <div className="flex justify-center -translate-y-[1px]">
@@ -38,7 +38,7 @@ function Projects() {
         <Link
           className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
           role="button"
-          href="/blog"
+          href="/project"
         >
           <span>View More</span>
           <FaArrowRight size={16} />
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -27,13 +27,14 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
           </li>
           <li>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/project"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+          </li>
+          <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
           </li>
+          
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#achivements"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ACHIVEMENTS</div></Link>
           </li>
         </ul>
       </div>
